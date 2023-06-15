b059e84ab42f3231f92995410eee27f66b0caffe
Himanshu Mote <86561270+himanshumote03@users.noreply.github.com>
2023-06-15 17:41:33 +0530

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 
 class RepeatElement {
-    void repeatingNumbers(int arr[], int n) {
+    public static void repeatingNumbers(int arr[], int n) {
         int i, j;
-        System.out.print("Repeating Elements are ");
+        System.out.print("Repeating Elements are { ");
         for (i = 0; i < n - 1; i++) {
             for (j = i + 1; j < n; j++) {
                 if (arr[i] == arr[j])
                     System.out.print(arr[i] + " ");
             }
         }
+        System.out.println("}");
     }
 
     public static void main(String[] args) {
@@ -22,6 +23,6 @@ class RepeatElement {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        repeat.repeatingNumbers(arr, n);
+        repeatingNumbers(arr, n);
     }
 }
